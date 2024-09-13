d16f3f29b92b7645c1222a73a661637e969718d7
MINEDUCYT <MINEDUCYT@DESKTOP-7SD01TN>
2024-09-13 10:53:20 -0600

    Agregar archivos de proyecto.

---
+++ proyecto Progra1/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿using System;
+
+namespace proyecto_Progra1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose ( bool disposing )
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose ( );
+            }
+            base.Dispose ( disposing );
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent ( )
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.txtDato1 = new System.Windows.Forms.TextBox();
+            this.txtDato2 = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstOperacion = new System.Windows.Forms.ListBox();
+            this.lblOperacion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(150, 235);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(378, 235);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 1;
+            this.btnLimpiar.Text = "reiniciar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(616, 235);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCerrar.TabIndex = 2;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // txtDato1
+            // 
+            this.txtDato1.Location = new System.Drawing.Point(255, 166);
+            this.txtDato1.Name = "txtDato1";
+            this.txtDato1.Size = new System.Drawing.Size(100, 20);
+            this.txtDato1.TabIndex = 3;
+            this.txtDato1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDato1_KeyPress);
+            // 
+            // txtDato2
+            // 
+            this.txtDato2.Location = new System.Drawing.Point(497, 166);
+            this.txtDato2.Name = "txtDato2";
+            this.txtDato2.Size = new System.Drawing.Size(100, 20);
+            this.txtDato2.TabIndex = 4;
+            this.txtDato2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDato2_KeyPress);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(375, 166);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(99, 13);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "En espera de datos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(105, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(640, 48);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Calculadora de Operaciones Basicas";
+            // 
+            // lstOperacion
+            // 
+            this.lstOperacion.FormattingEnabled = true;
+            this.lstOperacion.Items.AddRange(new object[] {
+            "Suma",
+            "Resta",
+            "Multiplicacion",
+            "Division"});
+            this.lstOperacion.Location = new System.Drawing.Point(662, 113);
+            this.lstOperacion.Name = "lstOperacion";
+            this.lstOperacion.Size = new System.Drawing.Size(71, 56);
+            this.lstOperacion.TabIndex = 7;
+            this.lstOperacion.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // lblOperacion
+            // 
+            this.lblOperacion.AutoSize = true;
+            this.lblOperacion.Location = new System.Drawing.Point(637, 86);
+            this.lblOperacion.Name = "lblOperacion";
+            this.lblOperacion.Size = new System.Drawing.Size(156, 13);
+            this.lblOperacion.TabIndex = 8;
+            this.lblOperacion.Text = "asigne una operacion de la lista";
+            this.lblOperacion.Click += new System.EventHandler(this.lblOperacion_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 285);
+            this.Controls.Add(this.lblOperacion);
+            this.Controls.Add(this.lstOperacion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtDato2);
+            this.Controls.Add(this.txtDato1);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.TextBox txtDato1;
+        private System.Windows.Forms.TextBox txtDato2;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstOperacion;
+        private System.Windows.Forms.Label lblOperacion;
+        private EventHandler listBox1_SelectedIndexChanged;
+    }
+}
+
---
+++ proyecto Progra1/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyecto_Progra1
+{
+    public partial class Form1 : Form
+    {
+        public Form1 ( )
+        {
+            InitializeComponent ( );
+        }
+
+        private void button1_Click ( object sender, EventArgs e )
+        {
+
+            
+            //declaramos variables
+        
+            double dato1, dato2,resultado;
+
+            
+            //condicionamos excepciones para que no queden campos vacios
+            if (string.IsNullOrEmpty ( txtDato1.Text ) || (string.IsNullOrEmpty ( txtDato2.Text )))
+            {
+                MessageBox.Show ( "ingrese datos en los campos","Error",MessageBoxButtons.OK,MessageBoxIcon.Error );
+                return;
+            }
+            //asignamos valores a las variables
+
+            dato1 = Convert.ToDouble ( txtDato1.Text );
+            dato2 = Convert.ToDouble ( txtDato2.Text );
+
+
+
+            //condicional para elegir la operacion a realizar
+            
+            int indice = lstOperacion.SelectedIndex;
+            if (indice == 0)
+            {
+                resultado = dato1 + dato2;
+                lblResultado.Text = (Convert.ToString ( "el Resultado es :" + resultado ));
+            }else if (indice == 1)
+            {
+                resultado = dato1 - dato2;
+                lblResultado.Text = (Convert.ToString ( "el Resultado es :" + resultado ));
+            }else if (indice == 2)
+            {
+                resultado = dato1 * dato2;
+                lblResultado.Text = (Convert.ToString ( "el Resultado es :" + resultado ));
+            }else if (indice == 3)
+            {
+                resultado = dato1 / dato2;
+                lblResultado.Text = (Convert.ToString ( "el Resultado es :" + resultado ));
+            }
+            else //este else nos envia un mensaje para no generar una excepcion por no haber marcado ningun operador en el listbox
+            {
+                MessageBox.Show ( "seleccione la operacion a realizar","Error",MessageBoxButtons.OK,MessageBoxIcon.Error );
+            }
+           
+       
+
+        }
+
+
+        
+        private void Form1_Load ( object sender, EventArgs e )
+        {
+            //asignamos texto a los label para hacer mas dinamico el programa
+            lblResultado.Text = "En espera de datos";
+            lblOperacion.Text = "Asigne Una Operacion de La Lista";
+        }
+
+        private void btnLimpiar_Click ( object sender, EventArgs e )
+        {
+            // con este metodo vamos a reiniciar los valores para un nuevo registro
+            txtDato1.Clear ( );
+            txtDato2.Clear ( );
+            lblResultado.Text = "En espera de datos";
+            lstOperacion.ClearSelected();
+
+        }
+
+       
+        private void btnCerrar_Click ( object sender, EventArgs e )
+        {
+            // co esto mandamos a terminar el programa y cerrarlo
+            Close ( );
+        }
+
+        private void lblOperacion_Click(object sender, EventArgs e)
+        {
+
+        }
+        //este metodo es creado para que en los campos de numeros no se permita letras y ningun otro simbolo, tampoco permite que se escriba el punto decimal mas de una vez
+        //con esto evitamos que se generen excepciones
+        public void UnPunto(KeyPressEventArgs e, String cadena)
+        {
+            bool bandera;
+            int contador = 0;
+            string caracter = "";
+            for (int n = 0; n < cadena.Length; n++)
+            {
+                caracter = cadena.Substring(n, 1);
+                if (caracter == ".")
+                {
+                    contador++;
+                }
+            }
+            if (contador == 0)
+            {
+                bandera = true;
+                if (e.KeyChar == '.' && bandera)
+                {
+                    bandera = false; //ya no acepta otro punto
+                    e.Handled = false;
+                }
+                else if (char.IsDigit(e.KeyChar))
+                {
+                    e.Handled = false;
+                }
+                else if (char.IsControl(e.KeyChar))
+                {
+                    e.Handled = false;
+                }
+                else
+                {
+                    e.Handled = true;
+                }
+            }
+            else
+            {
+                bandera = false;
+                e.Handled = true;
+                if (char.IsDigit(e.KeyChar))
+                {
+                    e.Handled = false;
+                }
+                else if (char.IsControl(e.KeyChar))
+                {
+                    e.Handled = false;
+                }
+                else
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+        //el evento keypress lo usamos para llamar el metodo un punto para que se ejecute en el campo txt
+        private void txtDato1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //desde el evento keypress vamos a llamar el codigo UnPunto que usaremos para evitar
+            //excepciones por digitar letras o valores no numericos en el textbox
+            UnPunto(e, txtDato1.Text);
+        }
+        //llamamos al mismo metod para este evento y que no genere excepciones
+        private void txtDato2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            UnPunto(e, txtDato2.Text);
+        }
+    }
+}
